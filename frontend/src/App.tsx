926f31e92e8d531e427f7429e2dfaf2808154392
AlexxanndR <alexandrov.alexandder@gmail.com>
2024-12-22 12:51:20 +0300

    Change app and main files

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
+import { ReactKeycloakProvider } from "@react-keycloak/web"
+import keycloak from "./Keycloak"
+import Router from "./components/router/Router"
 
 function App() {
-
   return (
-    <>
-    </>
+    <ReactKeycloakProvider authClient={keycloak}>
+      <Router />
+    </ReactKeycloakProvider>
   )
 }
 
